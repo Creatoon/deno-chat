f09a70270b0e19ad7f8d6a98db294017092bdc8f
Creatoon <yasharyan1099@gmail.com>
2022-02-12 19:52:42 +0530

    update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,9 +1,28 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const messages = [];
+// books.set("1", {
+//   id: "1",
+//   title: "The Hound of the Baskervilles",
+//   author: "Conan Doyle, Arthur",
+// });
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Chat server!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages;
+  })
+  .post("/messages", async(context) => {
+    const message = await context.request.body().value;
+    messages.push(message);
+    context.response.body = messages;
+  });
 
 const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
